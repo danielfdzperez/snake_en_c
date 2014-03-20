437c29e46b6b79252d5515a5f33c6282db098d09
danielfdzperez <daniel.fdz.perez@gmail.com>
2014-03-20 19:17:22 +0100

    programa inicial

---
+++ snake_v1.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdio_ext.h> //Se usa para _fpurge
+#include <time.h> //se usa para utilizar random
+#include <ncurses.h> 
+
+int main(int argc, char *argv[]){
+
+
+   return EXIT_SUCCESS;
+}
