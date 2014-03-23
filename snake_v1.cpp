4be909e1504ff164179b45853ecb2b85509acd1c
danielfdzperez <daniel.fdz.perez@gmail.com>
2014-03-23 14:54:40 +0100

    hay comida, la serpiente se hace mas grande

--- snake_v1.cpp
+++ snake_v1.cpp
@@ -7,70 +7,137 @@
 #define FILAS 20
 #define COLUMNAS 20
 #define EXIT 27
+#define CARACTER 'x'
+
+void posicion_inicial(int pos_usuario[][2]){
+
+    pos_usuario[0][0] = 5;//fila
+    pos_usuario[0][1] = 3;//columna
+    pos_usuario[1][0] = 5;//fila
+    pos_usuario[1][1] = 4;//columna
+    pos_usuario[2][0] = 5;//fila
+    pos_usuario[2][1] = 5;//columna
+
+}
+
+void comida_aleatoria(char tablero[][COLUMNAS]){
+
+    int posicion_aleatoria[2];
+    bool posicion_encontrada = false;
+    do{
+	posicion_aleatoria[1] = random() % COLUMNAS;//columna
+	posicion_aleatoria[0] = random() % FILAS;//fila
+
+	if(tablero[posicion_aleatoria[0]][posicion_aleatoria[1]] != 'x' && 
+		tablero[posicion_aleatoria[0]][posicion_aleatoria[1]] != 'H'){
+	    posicion_encontrada = true;
+	    tablero[posicion_aleatoria[0]][posicion_aleatoria[1]] = '#';
+	}
+
+    }while(posicion_encontrada != true);
+
+}
+
+void inicializar_tablero(char tablero[][COLUMNAS], int pos_usuario[][2]){
+    //rellenar el tablero
+    for(int fila = 0; fila<FILAS; fila++)
+	for(int col=0; col<COLUMNAS; col++)
+	    if(col == 0 || col == COLUMNAS-1 || fila == 0 || fila == FILAS-1)
+		tablero[fila][col] = 'H';
+	    else
+		tablero[fila][col] = ' ';
+    //posiciones de la serpiente
+    tablero[pos_usuario[0][0]][pos_usuario[0][1]] = CARACTER;
+    tablero[pos_usuario[1][0]][pos_usuario[1][1]] = CARACTER;
+    tablero[pos_usuario[2][0]][pos_usuario[2][1]] = CARACTER;
+    comida_aleatoria(tablero);
+}
 
 int movimiento(int respuesta_anterior){
-	int respuesta;
-	//coge los caracteres introducidos por el usuario
-	halfdelay(5);
-	respuesta = getch();//coge los caracteres
-	if(respuesta != KEY_UP && respuesta != KEY_LEFT && respuesta != KEY_RIGHT && respuesta != KEY_DOWN && respuesta != EXIT)
-	    respuesta = respuesta_anterior;
-	else
-	    respuesta_anterior = respuesta;
-	return respuesta;
-    }
+    int respuesta;
+    //coge los caracteres introducidos por el usuario
+    halfdelay(3);
+    respuesta = getch();//coge los caracteres
+    if(respuesta != KEY_UP && respuesta != KEY_LEFT && respuesta != KEY_RIGHT && respuesta != KEY_DOWN && respuesta != EXIT)
+	respuesta = respuesta_anterior;
+    else
+	respuesta_anterior = respuesta;
+    return respuesta;
+}
 
-void movimiento_usuario(int direccion, int pos_usuario[][2], char tablero[][COLUMNAS]){
+void movimiento_usuario(int direccion, int pos_usuario[][2], char tablero[][COLUMNAS], bool *comido, int *longitud){
     switch(direccion){
 	case KEY_RIGHT:
-	    tablero[pos_usuario[2][0]][pos_usuario[2][1]] = ' ';
-	    for(int copiar = 2; copiar>0; copiar--){
+	    if(tablero[pos_usuario[0][0]][pos_usuario[0][1] + 1] == '#'){
+		*comido = true;
+		(*longitud)++;
+	    }
+	    else
+		tablero[pos_usuario[*longitud][0]][pos_usuario[*longitud][1]] = ' ';
+
+	    for(int copiar = *longitud; copiar>0; copiar--){
 		for(int c_pos = 0; c_pos<2; c_pos++)
 		    pos_usuario[copiar][c_pos] = pos_usuario[copiar-1][c_pos];
-		tablero[pos_usuario[copiar][0]][pos_usuario[copiar][1]] = 'x';
+		tablero[pos_usuario[copiar][0]][pos_usuario[copiar][1]] = CARACTER;
 	    }
 
 	    pos_usuario[0][1]++; 
-	    tablero[pos_usuario[0][0]][pos_usuario[0][1]] = 'x';
+	    tablero[pos_usuario[0][0]][pos_usuario[0][1]] = CARACTER;
 
 	    break;
 
-	    case KEY_LEFT:
-	    tablero[pos_usuario[2][0]][pos_usuario[2][1]] = ' ';
-	    for(int copiar = 2; copiar>0; copiar--){
+	case KEY_LEFT:
+	    if(tablero[pos_usuario[0][0]][pos_usuario[0][1] - 1] == '#'){
+		*comido = true;
+		(*longitud)++;
+	    }
+	    else
+		tablero[pos_usuario[*longitud][0]][pos_usuario[*longitud][1]] = ' ';
+
+	    for(int copiar = *longitud; copiar>0; copiar--){
 		for(int c_pos = 0; c_pos<2; c_pos++)
 		    pos_usuario[copiar][c_pos] = pos_usuario[copiar-1][c_pos];
-		tablero[pos_usuario[copiar][0]][pos_usuario[copiar][1]] = 'x';
+		tablero[pos_usuario[copiar][0]][pos_usuario[copiar][1]] = CARACTER;
 	    }
-
 	    pos_usuario[0][1]--; 
-	    tablero[pos_usuario[0][0]][pos_usuario[0][1]] = 'x';
+	    tablero[pos_usuario[0][0]][pos_usuario[0][1]] = CARACTER;
 
 	    break;
 
-	    case KEY_UP:
-	    tablero[pos_usuario[2][0]][pos_usuario[2][1]] = ' ';
-	    for(int copiar = 2; copiar>0; copiar--){
-		for(int c_pos = 0; c_pos<2; c_pos++)
-		    pos_usuario[copiar][c_pos] = pos_usuario[copiar-1][c_pos];
-		tablero[pos_usuario[copiar][0]][pos_usuario[copiar][1]] = 'x';
+	case KEY_UP:
+	    if(tablero[pos_usuario[0][0] - 1][pos_usuario[0][1]] == '#'){
+		*comido = true;
+		(*longitud)++;
 	    }
+	    else
+		tablero[pos_usuario[*longitud][0]][pos_usuario[*longitud][1]] = ' ';
 
+	    for(int copiar = *longitud; copiar>0; copiar--){
+		for(int c_pos = 0; c_pos<2; c_pos++)
+		    pos_usuario[copiar][c_pos] = pos_usuario[copiar-1][c_pos];
+		tablero[pos_usuario[copiar][0]][pos_usuario[copiar][1]] = CARACTER;
+	    }
 	    pos_usuario[0][0]--; 
-	    tablero[pos_usuario[0][0]][pos_usuario[0][1]] = 'x';
+	    tablero[pos_usuario[0][0]][pos_usuario[0][1]] = CARACTER;
 
 	    break;
 
-	    case KEY_DOWN:
-	    tablero[pos_usuario[2][0]][pos_usuario[2][1]] = ' ';
-	    for(int copiar = 2; copiar>0; copiar--){
+	case KEY_DOWN:
+	    if(tablero[pos_usuario[0][0] + 1][pos_usuario[0][1]] == '#'){
+		*comido = true;
+		(*longitud)++;
+	    }
+	    else
+		tablero[pos_usuario[*longitud][0]][pos_usuario[*longitud][1]] = ' ';
+
+	    for(int copiar = *longitud; copiar>0; copiar--){
 		for(int c_pos = 0; c_pos<2; c_pos++)
 		    pos_usuario[copiar][c_pos] = pos_usuario[copiar-1][c_pos];
-		tablero[pos_usuario[copiar][0]][pos_usuario[copiar][1]] = 'x';
+		tablero[pos_usuario[copiar][0]][pos_usuario[copiar][1]] = CARACTER;
 	    }
 
 	    pos_usuario[0][0]++; 
-	    tablero[pos_usuario[0][0]][pos_usuario[0][1]] = 'x';
+	    tablero[pos_usuario[0][0]][pos_usuario[0][1]] = CARACTER;
 
 	    break;
     }
@@ -95,31 +162,24 @@ int main(int argc, char *argv[]){
     noecho();
 
     char tablero[FILAS][COLUMNAS];
-    int pos_usuario[5][2];
+    int pos_usuario[100][2],
+	longitud = 2;//la longitud de la serpiente.
     int direccion;
+    bool comido = false;//determina si se ha comido o no comida
+    srand(time(NULL));
 
-    pos_usuario[0][0] = 5;//fila
-    pos_usuario[0][1] = 3;//columna
-    pos_usuario[1][0] = 5;//fila
-    pos_usuario[1][1] = 4;//columna
-    pos_usuario[2][0] = 5;//fila
-    pos_usuario[2][1] = 5;//columna
+    direccion = KEY_RIGHT;
+    posicion_inicial(pos_usuario);
 
-    //rellenar el tablero
-    for(int fila = 0; fila<FILAS; fila++)
-	for(int col=0; col<COLUMNAS; col++)
-	    if(col == 0 || col == COLUMNAS-1 || fila == 0 || fila == FILAS-1)
-		tablero[fila][col] = 'H';
-	    else
-		tablero[fila][col] = ' ';
-    //posiciones de la serpiente
-    tablero[pos_usuario[0][0]][pos_usuario[0][1]] = 'x';
-    tablero[pos_usuario[1][0]][pos_usuario[1][1]] = 'x';
-    tablero[pos_usuario[2][0]][pos_usuario[2][1]] = 'x';
+    inicializar_tablero(tablero, pos_usuario);
 
     do{
+	if(comido == true){
+	    comida_aleatoria(tablero);
+	    comido = false;
+	}
 	direccion = movimiento(direccion);//captura el movimiento del usuario
-	movimiento_usuario(direccion, pos_usuario, tablero);//mueve al usuario
+	movimiento_usuario(direccion, pos_usuario, tablero, &comido, &longitud);//mueve al usuario
 	imprimir_tablero(tablero);
     }while(direccion != EXIT);
 
